ea9618b2e4730c738d53e6743985b9cef96e56ff
TofaelKabir <tofaelkabir@gmail.com>
2023-10-11 17:53:02 -0400

    update

---
+++ src/lec19_java_arrayList/UseOfArrayList03.java
@@ -0,0 +1,43 @@
+package lec19_java_arrayList;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class UseOfArrayList03 {
+
+	public static void main(String[] args) {
+
+		ArrayList ar = new ArrayList(); // warning for not using generic data type
+		ar.add("Alex"); // index [0] 
+		ar.add("Michael");
+		ar.add("Jennifer"); // index[2]
+		ar.add("Brian");
+		ar.add(45); 
+		ar.add(false);
+		ar.add(""); 
+		ar.add(3, "Alison"); // Assign Alison to index 3, before going to line 25
+		ar.add("Alfred");
+		ar.add("Jeff");
+		ar.add(5, 34.556); // Important re-indexing
+		ar.add("Michael");
+		ar.add('M'); // Here char type
+		
+		// New here
+		// remove() --> Removes the element at the specified position in this list. Shifts any subsequent elements to the left (subtracts one from their indices).
+		ar.remove(2); // Remove the index number 2 after all function till line 23
+		ar.remove(9); // below index 5 will not remove before index 9 of this line
+		ar.remove(5);
+		
+		System.out.println("The size of the ArrayList is: " + ar.size() + "\n"); // In Array this is length
+		System.out.println("\n All the value of ArrayList: " + ar); //ArrayList accept all data type
+		
+		System.out.println("^^^^^^^^^^^^^ Outcome ^^^^^^^^^^^^^^^^ ");
+		System.out.println("Single index value: " + ar.get(0)); // output:
+		System.out.println("Single index value: " + ar.get(1)); // output:
+		System.out.println("Single index value: " + ar.get(4)); // output:
+		System.out.println("Single index value: " + ar.get(6)); // output:
+		System.out.println("Last index value: " + ar.get(ar.size()-1));
+
+	}
+
+}
--- src/lec19_java_arrayList/UseOfArrayList06.java
+++ src/lec19_java_arrayList/UseOfArrayList06.java
@@ -6,27 +6,58 @@ import java.util.Iterator;
 public class UseOfArrayList06 {
 
 	public static void main(String[] args) {
-		StudyGroup s1 = new StudyGroup("Ameer", 34, "Khan");
-		StudyGroup s2 = new StudyGroup("Robert", 54, "Nero");
-		StudyGroup s3 = new StudyGroup("Anjelina", 44, "Jollie");
-		StudyGroup s4 = new StudyGroup("Demi", 64, "Moore");
+		ArrayList ar = new ArrayList(); // warning for not using generic data type
+		ar.add("Alex"); // index [0] 
+		ar.add("Michael");
+		ar.add("Jennifer"); // index[2]
+		ar.add("Brian");
+		ar.add(45); 
+		ar.add(false);
+		ar.add(""); 
+		ar.add(3, "Alison"); // Assign Alison to index 3, before going to line 25
+		ar.add("Alfred");
+		ar.add("Jeff");
+		ar.add(5, 34.556); // Important re-indexing
+		ar.add('M'); // Here char type
+		System.out.println(ar.size()); // In Array this is length
+		System.out.println(ar); //ArrayList accept all data type
 		
-		ArrayList<StudyGroup>ar = new ArrayList<>();
-		ar.add(s1);
-		ar.add(s2);
-		ar.add(s3);
-		ar.add(s4);
+		// Use of addAll()
+		ArrayList <String> arrayList = new ArrayList<>();
+		arrayList.add("A");
+		arrayList.add("B");
+		arrayList.add("C");
+		arrayList.add("D");
+		arrayList.add("E");
 		
-		// print by using for each loop/enhanced for loop/advanced for loop
-		System.out.println("\n------------- Accessing the elements of the specified arrayList by using for each loop -------------");
-		for (StudyGroup sg : ar) { //sg is a container, where to put the value from the below loop
-			System.out.println(sg.fname + " " + sg.lname + " " + sg.age);
-		}
+		// New
+		// How can we get the content of one ArrayList inside Another
+		// addAll() --> Appends all of the elements in the specified collection to the end of this list, in the order that they are returned by the specified collection's Iterator.
+		// ar object from line 9
+		ar.addAll(arrayList);
+		// applied addAll() in ar[]-->now ar object have the content of the arrrayList object
+		System.out.println(ar.size());
+		System.out.println("After using addAll method the new Array List: \n" + ar);
 		
+		// use of removeAll() method
+		// removeAll() --> Removes from this list all of its elements that are contained in the specified collection.
+		ar.removeAll(arrayList);
+		System.out.println(ar.size());
+		System.out.println("After using removeAll method the new Array List: \n" + ar);
 		
+		// use of retainAll()--only keeps common value between two ArrayList obj
+		ArrayList<String> ar9 = new ArrayList(); // creating new ArrayList
+		ar9.add("Alex"); // -------this is common
+		ar9.add("Brian");
+		ar9.add("alfred"); // Case sensitive
+		ar9.add("Jennifer"); 
+		// applying retainAll() below, which will keep the common from the list
+		// Retains only the elements in this list that are contained in the specified collection. In other words, removes from this list all of its elements that are not contained in the specified collection.
+		ar.retainAll(ar9);
 		
+		System.out.println(ar.size());
+		System.out.println("After using retainAll method the new Array List: \n" + ar);
 		
-
 	}
 
 }
